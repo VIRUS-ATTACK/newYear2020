5c799b85a4fe4aa14294df92e137d76c32ef553c
surya teja <surya@gmail.com>
2019-12-31 20:52:14 +0530

    small changes

--- mysite/newyear/views.py
+++ mysite/newyear/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request,username = 'your friend'):
-    context = {'usernamename':username}
+    context = {'username':username}
     return render(request,'newyear/files/wishes.html',context)
     #return HttpResponse("Happy new Year 2020")
