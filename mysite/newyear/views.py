40dad4543c9fc4b3e6f9febfb5445e57f071f013
surya teja <surya@gmail.com>
2019-12-31 20:04:47 +0530

    added username

--- mysite/newyear/views.py
+++ mysite/newyear/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    return render(request,'newyear/files/wishes.html')
+def index(request,username = 'your friend'):
+    context = {'user':username}
+    return render(request,'newyear/files/wishes.html',context)
     #return HttpResponse("Happy new Year 2020")
